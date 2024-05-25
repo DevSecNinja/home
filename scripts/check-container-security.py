1e3b8a2ada918838f58798a9dbf9366305fc2306
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2024-05-26 00:59:21 +0200

    Create JSON first, then table output

--- scripts/check-container-security.py
+++ scripts/check-container-security.py
@@ -17,9 +17,13 @@ unique_images = set()
 for container in docker_containers:
     image = container['image']
     container_name = container['container_name']
-    output_file = 'docker/security-reports/' + container_name + ".txt"
+    output_file = 'docker/security-reports/' + container_name + ".json"
+    output_table_file = 'docker/security-reports/' + container_name + ".txt"
 
     if image not in unique_images:
         unique_images.add(image)
         print(f"Checking {image} for security vulnerabilities...")
-        subprocess.run(['trivy', 'image', '--scanners', 'vuln', '--severity', 'HIGH,CRITICAL', '--format', 'table', '--output', output_file, image])
+        subprocess.run(['trivy', 'image', '--scanners', 'vuln', '--severity', 'HIGH,CRITICAL', '--format', 'json', '--output', output_file, image])
+        print('Converting JSON output to table')
+        subprocess.run(['trivy', 'convert', '--format', 'table', '--output', output_table_file, output_file])
+
